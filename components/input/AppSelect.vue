11d1fbbe10f30738d4da168165826676872e3ceb
Thamyres Guedes <thataguedis@gmail.com>
2017-12-10 11:19:48 -0200

    🚧 Form select component

---
+++ components/input/AppSelect.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="input-field col s12">
+     <select
+        v-bind="$props"
+        :id="id"
+        @blur="setActivity(false)"
+        @focus="setActivity(true)"
+      >
+    <label :for="id" class="active">
+      <slot>
+        {{ label }}
+      </slot>
+    </label>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'AppSelect',
+    data () {
+      return {
+        isActive: false
+      }
+    },
+    props: {
+      id: String,
+      items: Array,
+    },
+    computed: {
+      activityStatus () {
+        return this.isActive || this.value ? 'active' : ''
+      }
+    },
+    methods: {
+      setActivity (state) {
+        this.isActive = state
+      }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
--- pages/proccess/save.vue
+++ pages/proccess/save.vue
@@ -2,25 +2,25 @@
   AppCard(style="margin-top: 48px; flex: 100%")
     section.card-content
       AppText(label="Nome do processo")
-
-      .select-group
-        label
-        select
-          each value in [1,2,3,4,5]
-            option= value
-        
+      AppSelect(id="select-area" items="getItems(false)")
 </template>
 
 <script>
   import AppCard from '~/components/AppCard.vue'
-  import { AppButton, AppText } from '~/components/input'
+  import { AppButton, AppText, AppSelect } from '~/components/input'
 
   export default {
-    name: 'create',
+    name: 'save',
     components: {
       AppCard,
       AppButton,
+      AppSelect,
       AppText
+    },
+    computed: {
+      getItems () {
+        return [1, 2, 3, 4, 5]
+      }
     },
     mounted () {
       this.$store.dispatch('layout/setStyle', {
